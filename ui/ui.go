f3ba358f9a2a88d368f4770529cc54ac8268964b
Florian Thienel <ftl@thecodingflow.com>
2020-04-26 15:23:07 +0200

    use gtk.Application, fix the mess with the stored x/y coordinates of the window

--- ui/ui.go
+++ ui/ui.go
@@ -1,14 +1,16 @@
 package ui
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/ftl/gmtry"
 	"github.com/ftl/hamradio/cfg"
 	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
-	"github.com/pkg/errors"
 )
 
 type Application interface {
@@ -19,61 +21,80 @@ type Application interface {
 	ToggleTuning() (bool, error)
 }
 
-func Run(app Application) error {
-	gtk.Init(nil)
+type ui struct {
+	application Application
+	gtkapp      *gtk.Application
+	mainWindow  *gtk.ApplicationWindow
+	geometry    *gmtry.Geometry
+}
 
-	mainWindow, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+func (ui *ui) onActivate() {
+	var err error
+	ui.mainWindow, err = gtk.ApplicationWindowNew(ui.gtkapp)
 	if err != nil {
-		return errors.Wrap(err, "cannot create main window")
+		log.Fatalf("Cannot create main window: %v", err)
 	}
-	mainWindow.SetTitle("Power Switch")
-	mainWindow.Connect("destroy", func() {
-		gtk.MainQuit()
-	})
+	ui.mainWindow.SetTitle("Power Switch")
 
 	grid, err := gtk.GridNew()
 	if err != nil {
-		return errors.Wrap(err, "cannot create grid")
+		log.Fatalf("Cannot create grid: %v", err)
 	}
 	grid.SetOrientation(gtk.ORIENTATION_HORIZONTAL)
 	grid.SetHExpand(true)
 
-	for i, level := range app.PowerLevels() {
+	for i, level := range ui.application.PowerLevels() {
 		button, err := gtk.ButtonNewWithLabel(level)
 		if err != nil {
-			return errors.Wrap(err, "cannot create button")
+			log.Fatalf("Cannot create button: %v", err)
 		}
-		button.Connect("clicked", powerSetter(app, i))
+		button.Connect("clicked", powerSetter(ui.application, i))
 		grid.Add(button)
 	}
 
 	rxButton, _ := gtk.ButtonNewWithLabel("Rx")
-	rxButton.Connect("clicked", txSetter(app, false))
+	rxButton.Connect("clicked", txSetter(ui.application, false))
 	grid.Add(rxButton)
 	tuneButton, _ := gtk.ButtonNewWithLabel("Tune")
-	tuneButton.Connect("clicked", tuningToggler(app, tuneButton))
+	tuneButton.Connect("clicked", tuningToggler(ui.application, tuneButton))
 	tuneButton.SetHExpand(true)
 	grid.Add(tuneButton)
 
-	mainWindow.Add(grid)
+	ui.mainWindow.Add(grid)
+	ui.mainWindow.SetDefaultSize(100, 100)
+
+	connectToGeometry(ui.geometry, "main", &ui.mainWindow.Window)
+	err = ui.geometry.Restore()
+	if err != nil {
+		log.Printf("Cannot restore the window geometry: %v", err)
+	}
+
+	ui.mainWindow.ShowAll()
+}
+
+func Run(app Application) error {
+	var err error
+
+	gdk.SetAllowedBackends("x11")
+
+	ui := new(ui)
+	ui.application = app
+	ui.gtkapp, err = gtk.ApplicationNew("ft.pwrswtch", glib.APPLICATION_FLAGS_NONE)
+	if err != nil {
+		return fmt.Errorf("cannot create application: %v", err)
+	}
+	ui.gtkapp.Connect("activate", ui.onActivate)
 
 	configDir, err := cfg.Directory("")
 	if err != nil {
 		log.Fatalf("No access to configuration directory %s: %v", cfg.DefaultDirectory, err)
 	}
 	filename := filepath.Join(configDir, "pwrstch.geometry")
-	geometry := gmtry.NewGeometry(filename)
-	connectToGeometry(geometry, "main", mainWindow)
-	err = geometry.Restore()
-	if err != nil {
-		log.Printf("Cannot restore the window geometry: %v", err)
-	}
+	ui.geometry = gmtry.NewGeometry(filename)
 
-	mainWindow.ShowAll()
+	ui.gtkapp.Run(os.Args)
 
-	gtk.Main()
-
-	err = geometry.Store()
+	err = ui.geometry.Store()
 	if err != nil {
 		log.Printf("Cannot store the window geometry: %v", err)
 	}
@@ -108,8 +129,8 @@ func connectToGeometry(geometry *gmtry.Geometry, id gmtry.ID, window *gtk.Window
 
 	window.Connect("configure-event", func(_ interface{}, event *gdk.Event) {
 		e := gdk.EventConfigureNewFromEvent(event)
-		w := geometry.Get(id)
-		w.SetPosition(e.X(), e.Y())
+		w := geometry.Get(id)		
+		w.SetPosition(window.GetPosition()) // in this setup, the event contains the y of the area below the title bar, not the y of the top-left corner
 		w.SetSize(e.Width(), e.Height())
 	})
 	window.Connect("window-state-event", func(_ interface{}, event *gdk.Event) {
